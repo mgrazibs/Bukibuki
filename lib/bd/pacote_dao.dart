a96f9c7346e7837cf121657965fc537fb9772e11
Gabriel <gsa15@aluno.ifal.edu.br>
2024-12-16 22:08:22 -0300

    Gabriel: Alteração na tela pacote_dao.dart

--- lib/bd/pacote_dao.dart
+++ lib/bd/pacote_dao.dart
@@ -1,36 +1,25 @@
-import 'dart:async';
-
-import 'package:path/path.dart';
+import 'package:myapp/bd/db_helper.dart';
+import 'package:myapp/Domain/domain.dart';
 import 'package:sqflite/sqflite.dart';
 
-class DBHelper {
-  Future<Database> initDB() async {
-    String path = await getDatabasesPath();
-    String dbPath = join(path, 'pacote.db');
-    Database db = await openDatabase(
-      dbPath,
-      version: 1,
-      onCreate: onCreate,
-    );
-
-    return db;
+class PacoteDao {
+  salvarPacote(CompraLivro compralivro) async {
+    Database database = await DBHelper().initDB();
+    database.insert('PACOTE', compralivro.toJson());
   }
 
-  FutureOr<void> onCreate(Database db, int version) async {
-    String sql =
-        'CREATE TABLE PACOTE (id INTEGER PRIMARY KEY AUTOINCREMENT, urlImage varchar(100), titulo varchar(100), cont varchar(100), valor varchar(100));';
-    await db.execute(sql);
+  listarPacotes() async {
+    Database database = await DBHelper().initDB();
 
-    sql =
-    "INSERT INTO PACOTE (id, urlImage, titulo, cont, valor) VALUES (1, 'https://m.media-amazon.com/images/I/81PQNhm2shL._AC_UF1000,1000_QL80_.jpg', 'MYSTICAL', 1, 220.90);";
-    await db.execute(sql);
+    String sql = 'SELECT * FROM PACOTE;';
+    var result = await database.rawQuery(sql);
 
-    sql =
-    "INSERT INTO PACOTE (id, urlImage, titulo, cont, valor) VALUES (2, 'https://m.media-amazon.com/images/I/91N9kjbqxWS._AC_UF1000,1000_QL80_.jpg', 'O REI PERVERSO', 1, 30);";
-    await db.execute(sql);
+    List<CompraLivro> lista = [];
+    for (var json in result) {
+      CompraLivro pacote = CompraLivro.fromJson(json);
+      lista.add(pacote);
+    }
 
-    sql =
-    "INSERT INTO PACOTE (id, urlImage, titulo, cont, valor) VALUES (3, 'https://m.media-amazon.com/images/I/815Xccj7IRL._AC_UF1000,1000_QL80_.jpg', 'O REINO SECRETO', 1, 10);";
-    await db.execute(sql);
+    return lista;
   }
 }
