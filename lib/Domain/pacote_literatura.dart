789b260de0fc5b07e9f6fc6bd5bb9cdbc4e8c8b0
mgrazibs <mgbs5@aluno.ifal.edu.br>
2024-12-23 09:09:16 -0300

    Update and rename db_helper.dart to pacote_literatura.dart

---
+++ lib/Domain/pacote_literatura.dart
@@ -0,0 +1,12 @@
+class PacoteLiteratura {
+  String urlImage;
+  String titulo;
+  String descricao;
+
+  PacoteLiteratura({
+    required this.urlImage,
+    required this.titulo,
+    required this.descricao,
+  }
+  );
+}
